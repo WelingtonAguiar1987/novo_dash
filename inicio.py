b5743e3dad5012b31df93deb42ba93bff2f8ac70
WelingtonAguiar1987 <119814308+WelingtonAguiar1987@users.noreply.github.com>
2024-02-12 12:37:34 -0300

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+with st.container():
+    st.title('Anaálise de Volatilidade do Nasdaq Futuros')
+    st.image('CME_Group_and_Nasdaq_Logo.JPG')
+    st.text('Introdução: Esse dashboard serve para fazer cálculos estatísticos para nos orientar qual \na volatilidade média do ativo em questão. Através dele, podemos ter uma "estimativa" de \nonde "provavelmente", o ativo poderá realizar correções, alvos ou até mesmo stops.')
+    st.subheader('Criador: Welington Monteiro Aguiar')
